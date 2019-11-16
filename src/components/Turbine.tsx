29771f9c2fb047849876ad00e9f6c3bc31f1d1d5
Zoey Zou <13701719+zoeyzou@users.noreply.github.com>
2019-11-16 21:56:12 +0100

    Add turbine

---
+++ src/components/Turbine.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { useDrag } from "react-dnd";
+import styled from "styled-components";
+
+import turbine from "../assets/turbine.svg";
+import ItemTypes from "../types/item-types";
+
+const TurbineWrapper = styled.div<{ isDragging?: boolean }>`
+  width: 100%;
+  height: 100%;
+  max-width: 50px;
+  max-height: 50px;
+
+  img {
+    object-fit: contain;
+    max-width: 100%;
+    max-height: 100%;
+  }
+`;
+
+export const Turbine = () => {
+  const [{ opacity }, drag] = useDrag({
+    item: { type: ItemTypes.TURBINE },
+    begin: monitor => {
+      console.log({ monitor: monitor.getHandlerId() });
+    },
+    collect: monitor => ({
+      opacity: monitor.isDragging() ? 0.5 : 1
+    })
+  });
+
+  return (
+    <TurbineWrapper ref={drag} style={{ opacity }}>
+      <img src={turbine} />
+    </TurbineWrapper>
+  );
+};
