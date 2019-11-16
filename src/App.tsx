244feaff8d8ad5b1a14da878a8e5ad1dde851146
Zoey Zou <13701719+zoeyzou@users.noreply.github.com>
2019-11-16 16:11:14 +0100

    Implement basic grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { getMap } from "./utils/getMap";
+import { MapContextProvider, X, Y } from "./context/mapContext";
+import { Grid } from "./components/Grid";
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MapContextProvider
+      value={{
+        map: getMap(X, Y),
+        powerStorage: {
+          x: Math.floor(X / 2),
+          y: Math.floor(Y / 2)
+        }
+      }}
+    >
+      <Grid />
+    </MapContextProvider>
   );
-}
+};
 
 export default App;
---
+++ src/components/Grid.tsx
@@ -0,0 +1,37 @@
+import React, { FC, useContext } from "react";
+import styled from "styled-components";
+
+import { Grid as GridType, MapContext, X, Y } from "../context/mapContext";
+
+const StyledGrid = styled.main`
+  width: 500px;
+  height: 500px;
+  display: grid;
+  grid-template-columns: repeat(${X}, 1fr);
+  grid-template-rows: repeat(${Y}, 1fr);
+`;
+
+const StyledGridItem = styled.div<{ grid: GridType }>`
+  border: 1px solid grey;
+  background-color: ${props => (props.grid.canPlace ? "white" : "red")};
+`;
+
+export const Grid: FC = () => {
+  const { map } = useContext(MapContext);
+
+  return (
+    <StyledGrid>
+      {map.map((grid, index) => {
+        return <GridItem grid={grid} key={index} />;
+      })}
+    </StyledGrid>
+  );
+};
+
+export const GridItem: FC<{ grid: GridType }> = ({ grid }) => {
+  return (
+    <StyledGridItem grid={grid}>
+      {grid.canPlace && grid.placingCost}
+    </StyledGridItem>
+  );
+};
